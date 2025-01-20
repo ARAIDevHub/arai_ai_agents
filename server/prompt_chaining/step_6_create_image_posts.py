e52ddd23713df5079829026086182faa358c1a27
TheBlockRhino <theblockrhino@gmail.com>
2025-01-20 13:09:32 -0800

    Have the social media image posts working in a standalone manner

--- server/prompt_chaining/step_6_create_image_posts.py
+++ server/prompt_chaining/step_6_create_image_posts.py
@@ -25,7 +25,7 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # custom ARAI imports
-from utils.content_generator_json import ContentGenerator
+from utils.content_generator import ContentGenerator
 from utils.template_types import TemplateType
 import asset_generation.images_leonardo as images_leonardo
 from models.gemini_model import GeminiModel
@@ -33,7 +33,7 @@ from models.gemini_model import GeminiModel
 dotenv.load_dotenv()
 
 
-def create_image_posts(ai_model, master_file_path, num_images):
+def create_image_posts(ai_model, master_file_path, num_images, season_number, episode_number):
     manager = ContentGenerator()
     image_descriptions = None
 
@@ -44,43 +44,30 @@ def create_image_posts(ai_model, master_file_path, num_images):
 
     # step 4.3: extract agent from master json
     agent_details = agent_master_json['agent']['agent_details']
+    episode_details = agent_master_json['agent']['seasons'][season_number]['episodes'][episode_number]
 
     print("Crafting prompt for AI to create a new profile image descriptions")
-    prompt_4_vars = {
+    prompt_6_vars = {
         "agent_name": agent_details["name"],
-        "agent_json": json.dumps(agent_details),        
+        "agent_json": json.dumps(agent_details),
+        "episode_json": json.dumps(episode_details),
         "number_of_images": num_images
     }
 
     # step 2.4: Run the prompt 
     print("Sending prompt to AI to create a new profile image descriptions")
     image_descriptions = manager.run_prompt(
-        prompt_key="prompt_4 (Agent Profile Image)",
-        template_vars=prompt_4_vars, 
+        prompt_key="prompt_6 (Create Social Media Image Post)",
+        template_vars=prompt_6_vars, 
         ai_model=ai_model,
     )
 
     # print(f"image_descriptions is: {image_descriptions}")
     return image_descriptions
 
-#--------------------------------
-# Step 4: Generate a number of images for profile image
-#--------------------------------
-def create_profile_images(prompt, master_file_path, model_id, style_uuid, num_images, consistent=False, max_retries=10, delay=5):
-    
-    # step 4.1: load existing profile image options from templates directory
+def create_image_post(ai_model, prompt, model_id, style_uuid, num_images, consistent=False, max_retries=10, delay=5):
     manager = ContentGenerator()
-    profile_image_options_template = manager.create_new_template_json(TemplateType.PROFILE_IMAGE_OPTIONS)
-    profile_image_template = manager.create_new_template_json(TemplateType.PROFILE_IMAGE)
 
-    # step 4.2: load the agent json file
-    agent_master_json = None    
-    with open(master_file_path, 'r', encoding='utf-8') as file:
-        agent_master_json = json.load(file)  
-
-    # step 4.3: extract agent from master json
-    agent_details = agent_master_json['agent']['agent_details']
-    
     # step 4.4: generate the image
     if consistent:
         response, payload = images_leonardo.generated_image_consistent(prompt, model_id, style_uuid, num_images)
@@ -103,37 +90,15 @@ def create_profile_images(prompt, master_file_path, model_id, style_uuid, num_im
     
     if not success:
         print("Max retries reached. Image generation may have failed.")
-        return None
-            
-    # step 4.6: append new URL to the array
-    profile_image_options_template["profile_image_options"].append(response_url)
-    profile_image_template["profile_image"]["payload"] = payload
-    profile_image_template["profile_image"]["details"]["generationId"] = generation_id
+        return None           
 
     # step 4.7: create the save path, use 0 as we are not in a season or episode
-    save_path = manager.create_filepath(agent_details["name"], 0, 0, TemplateType.PROFILE_IMAGE_OPTIONS)
-    save_path_profile_image = manager.create_filepath(agent_details["name"], 0, 0, TemplateType.PROFILE_IMAGE)
+    save_path = os.path.join("configs", "temporary", "image_generations", f"generation_{generation_id}.json")
 
     # step 4.8: save the profile image options to a file
-    manager.save_json_file(save_path, profile_image_options_template)
-    manager.save_json_file(save_path_profile_image, profile_image_template)
+    manager.save_json_file(save_path, response_url)
 
-    # step 4.9: append to master file
-    agent_master_json = manager.append_profile_image_options(agent_master_json, profile_image_options_template)
-    agent_master_json = manager.append_profile_image(agent_master_json, profile_image_template)
-
-    # step 4.10: save the master data to a file
-    print("Saving the master data to a file")
-    manager.save_json_file(
-        save_path=master_file_path,
-        json_data=agent_master_json
-    )
-
-
-if __name__ == "__main__":
-    # Setup prompt
-    prompt = "Anime character Nicki. Generate a traditional looking anime character for me. I want her to be an anime girlfriend who is super into the crypto space. Make her attractive with big boobs. I want to see the big boobs clearly. Have her with hair green, eye color blue, skin color tan."
-    
+if __name__ == "__main__":    
     # Setup model details
     model_details = json.loads(open("asset_generation/leonard_anime_styles.json", "r").read())
     model_id = model_details["models"][0]["modelId"]
@@ -141,10 +106,12 @@ if __name__ == "__main__":
         style for style in model_details["models"][0]["styles"] 
         if style["style name"] == "Anime General"
     )
-    style_uuid = anime_general_style["styleUUID"]
 
-    # Setup number of images
-    num_images = 4
+    # Setup parameters
+    episode_number = 0
+    season_number = 0
+    style_uuid = anime_general_style["styleUUID"]    
+    num_images = 2
 
     # Setup AI model
     ai_model = GeminiModel()
@@ -153,18 +120,30 @@ if __name__ == "__main__":
     #--------------------------------
     # Step 4.1: Create multiple profile images via multiple prompts using consistent images
     #--------------------------------
-    image_descriptions = image_post_description(ai_model, master_file_path, num_images)
+    image_descriptions = create_image_posts(
+        ai_model=ai_model, 
+        master_file_path=master_file_path, 
+        num_images=num_images, 
+        episode_number=episode_number, 
+        season_number=season_number
+    )
 
     print("\n\n")    
-    print(f"image_descriptions is: {image_descriptions}")
+    print(f"image_descriptions is: {json.dumps(image_descriptions, indent=4)}")
     print("\n\n")
     
     # print the image descriptions
-    for image_description in image_descriptions["profile_image_descriptions"]:
-        print(image_description["description"] + "\n")
+    for image_description in image_descriptions["image_post_descriptions"]:
+        print(image_description["image_description"] + "\n")
+        print(image_description["post_after"] + "\n")
         # Generate the image
-        # step_4(image_description, master_file_path, model_id, style_uuid, num_images, consistent=False, max_retries=10, delay=5)
-    # Generate the image
-
-
-    
+        create_image_post(
+            ai_model=ai_model, 
+            prompt=image_description["image_description"], 
+            model_id=model_id, 
+            style_uuid=style_uuid, 
+            num_images=1, 
+            consistent=False, 
+            max_retries=10, 
+            delay=5
+        )
