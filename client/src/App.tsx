e3eaeaec2060df348edd8ff059823ac56421ba9a
TheBlockRhino <theblockrhino@gmail.com>
2025-02-03 09:38:55 -0800

    Hiding the social media mangers

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -90,7 +90,7 @@ function App() {
           >
             Chat to Agent
           </Link>
-          <Link
+{/*           <Link
             to="/social-dashboard"
             className={`text-lg font-semibold transition duration-300 px-4 py-2 rounded-lg
               ${
@@ -100,7 +100,7 @@ function App() {
               }`}
           >
             Social Dashboard
-          </Link>
+          </Link> */}
 
 {/*           <Link
             to="/token-launch"
